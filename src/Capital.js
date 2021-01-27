3c19998930bd15a082ec5e3b0763efeb264b395a
dav <dav@gelicon.biz>
2021-01-27 20:20:32 +0500

    инициализация проекта

---
+++ src/Capital.js
@@ -0,0 +1,12 @@
+function Capital() {
+  return (
+    <div className="Capital">
+      <header className="Capital-header">
+        <p>
+			Капитал
+        </p>
+      </header>
+    </div>
+  );
+}
+export default Capital;
