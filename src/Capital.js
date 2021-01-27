ad6837614d1c2b2e4abac742e63faadbbaf77b5f
dav <dav@gelicon.biz>
2021-01-27 22:14:12 +0500

    Развитие

--- src/Capital.js
+++ src/Capital.js
@@ -1,12 +1,15 @@
-function Capital() {
-  return (
-    <div className="Capital">
-      <header className="Capital-header">
-        <p>
-			Капитал
-        </p>
-      </header>
-    </div>
-  );
+import React from 'react';
+class Capital extends React.Component {
+  render() {
+    return (
+      <div className="Capital">
+        <header className="Capital-header">
+          <p>
+			      Капитал
+         </p>
+        </header>
+      </div>
+    );
+  }
 }
 export default Capital;
