301af5dd3a1090d661c060c9fc7abd39c1598b46
dav <dav@gelicon.biz>
2021-02-10 23:09:15 +0500

    component

--- src/Measure.js
+++ src/Measure.js
@@ -8,9 +8,9 @@ import reqwest from 'reqwest';
 import { PlusOutlined, EditOutlined, CloseOutlined, PrinterOutlined } from '@ant-design/icons';
 import Refresh from './icons/Refresh';
 import { notification } from 'antd';
-import { SmileOutlined } from '@ant-design/icons';
 import { ExclamationCircleOutlined } from '@ant-design/icons';
 import * as globalSettings from "./const";
+import { useCallback } from 'react';
 
 const { Header, Content, Footer } = Layout;
 
@@ -26,95 +26,24 @@ const columns = [
   },
 ];
 
-class Measure extends React.Component {
-  state = {
-    data: [],
-    pagination: {
-      current: 1,
-      pageSize: 10,
-    },
-    sorters: [{fieldName: "id", sortOrder: "ascend"}],
-    selectedRowKeys: [], 
-    loading: false,
-    totalMax: 0, // Наибольшее количесвто выбранных записей
-    currentMenu: null, // Текущее меню
-  };
-  // размещаем побочные эффекты
-  componentDidMount() {
-    console.log("componentDidMount - start")
-    const { pagination } = this.state;
-    this.fetch({ pagination });
-    console.log("componentDidMount - finish")
-  }
-  handleTableChange = (pagination, filters, sorter) => {
-    let { sorters } = this.state;
-    if (sorter.field) {
-      sorters = [
-        {
-          fieldName: sorter.field,
-          sortOrder: sorter.order
-        }
-      ];
-    }
-    this.fetch({
-      sorters,
-      pagination,
-      ...filters,
-    });
-  };
-  fetch = (params = {}) => {
-    console.log("fetch - start: " + globalSettings.startURL );
-    this.setState({ loading: true });
-    const gridDataOption = {
-      pageNumber: params.pagination.current - 1,
-      pageSize: params.pagination.pageSize,
-      sort: [{fieldName: "id"}]
-    };
-    // Из массива
-    if (params.sorters) {
-      gridDataOption.sort[0].fieldName = params.sorters[0].fieldName;
-      if (params.sorters[0].sortOrder === "descend") {
-        gridDataOption.sort[0].direction = 1;
-      }
-      this.setState({sorters:params.sorters} );
-    }
-    var total = (gridDataOption.pageNumber + 2) * gridDataOption.pageSize;
-    if (total < this.state.totalMax) {
-      total = this.state.totalMax;
-    } else {
-      this.setState({totalMax: total});
-    }
-    // ajax request after empty completing
-    reqwest({
-      url: globalSettings.startURL + "measure/json",
-      contentType: "application/json; charset=utf-8",
-      method: 'post',
-      type: 'json',
-      data:JSON.stringify(gridDataOption)
-    }).then(data => {
-      //console.log(data);
-      this.setState({
-        loading: false,
-        data: data,
-        pagination: {
-          ...params.pagination,
-          total: total,
-          // 200 is mock data, you should read it from server
-          // total: data.totalCount,
-        },
-        selectedRowKeys: [], // снимем все отметки
-      });
-      console.log("fetch - finish");
-    });
-  };
+const Measure = (props)=>{
+  let [currentMenu, setCurrentMenu] = React.useState("null");
+  let [selectedRowKeys, setSelectedRowKeys] = React.useState([]);
+  let [data,setData] = React.useState(null);
+  let [loading,setLoading] = React.useState(false);
+  let [pagination,setPagination] = React.useState({
+    current: 1,
+    pageSize: 10,
+    total: null,
+  });
+  let [sorters, setSorters] = React.useState([{
+    fieldName: "id", 
+    sortOrder: "ascend"
+  }]);
+  let [totalMax, setTotalMax] = React.useState(0); // Наибольшее количесвто выбранных записей
 
-  onSelectChange = selectedRowKeys => {
-    console.log('selectedRowKeys changed: ', selectedRowKeys);
-    this.setState({ selectedRowKeys });
-  };
 
-  deleteRows() {
-    const { selectedRowKeys } = this.state;
+  const deleteRows = () => {
     if (!(selectedRowKeys.length > 0)) {
       notification.info({
         message:"Нет записей для удаления",
@@ -124,16 +53,13 @@ class Measure extends React.Component {
     }
     let ids = selectedRowKeys.join(',');
     console.log('Deleting records ', ids, ' ...');
-    this.setState({ loading: true });
+    setLoading(true);
     reqwest({
       url: globalSettings.startURL + 'measure/del/' + ids,
       contentType: "application/json; charset=utf-8",
       method: 'post',
       type: 'json',
     }).then((responseJson) => {
-      this.setState({
-        loading: false
-      });
       console.log('responseJson=', responseJson);
       const { errorCode } = responseJson;
       if (errorCode) { // Ошибка
@@ -146,30 +72,27 @@ class Measure extends React.Component {
         });
         } else {
           console.log('Удалили');
-          this.refreshData();
+          refreshData();
           const description = "Удаление " + selectedRowKeys.length + " записей выполнено успешно";
           notification.success({
             message:"Успешно",
             description: (description)
           });
       }
+      setLoading(false);
       return;
     }).catch((error) => {
+      setLoading(false);
       throw (error);
     });
+    setLoading(false);
   }
 
-  refreshData() {
-    // Перевыборка текущей страницы
-    console.log('refreshData');
-    const { pagination, sorters } = this.state;
-    this.fetch({pagination, sorters});
-  }
-
-  handleMenuClick = e => {
+  const handleMenuClick = e => {
     console.log('click ', e);
     const { key } = e;
-    this.setState({ currentMenu: key });
+    currentMenu = key;
+    setCurrentMenu(setCurrentMenu);
     switch(key) {
       case 'add':
         console.log('add');
@@ -185,7 +108,6 @@ class Measure extends React.Component {
         });
         break;
       case 'delete':
-        const { selectedRowKeys } = this.state;
         if (!(selectedRowKeys.length > 0)) {
           notification.info({
             message:"Нет записей для удаления",
@@ -201,12 +123,12 @@ class Measure extends React.Component {
           okText: 'Да, конечно!',
           cancelText: 'Нет, я ошибся',
           onOk:()=>{
-            this.deleteRows();
+            deleteRows();
           }
         });
         break;
       case 'refresh':
-        this.refreshData();
+        refreshData();
         break;
       case 'print':
           notification.success({
@@ -219,64 +141,133 @@ class Measure extends React.Component {
     }
   };
 
-  render() {
-    console.log("render - start");
-    console.log("state=", this.state);
-    const { data, pagination, loading, selectedRowKeys} = this.state;
-    const rowSelection = {
-      selectedRowKeys,
-      onChange: this.onSelectChange,
+  console.log('Measure return');
+
+  const onSelectChange = selectedRowKeysNew => {
+    setSelectedRowKeys( selectedRowKeysNew );
+  };
+
+  const rowSelection = {
+    selectedRowKeys,
+    onChange: onSelectChange,
+  };
+
+  // При смене сортировки или страницы или фильтра
+  const handleTableChange = (paginationNew, filters, sorter) => {
+    console.log('handleTableChange - start');
+    console.log('paginationNew.current=' + paginationNew.current);
+    console.log('pagination.current=' + pagination.current);
+    console.log('pagination.pageSize=' + pagination.pageSize);
+    if (sorter.field) {
+      sorters = [{
+        fieldName: sorter.field, 
+        sortOrder: sorter.order
+      }];
+      setSorters(sorters);
+    }  
+    pagination = paginationNew;
+    setPagination(pagination);
+    refreshData();
+    console.log('handleTableChange - finish');
+  };
+
+  // Перевыборка
+  const refreshData = useCallback(() => {
+    // Перевыборка текущей страницы
+    console.log('refreshData - start');
+    setLoading(true);
+    const gridDataOption = {
+      pageNumber: pagination.current - 1,
+      pageSize: pagination.pageSize,
+      sort: [{fieldName: "id"}]
     };
-    const hasSelected = selectedRowKeys.length > 0;
-    const { currentMenu } = this.state; // Текущее меню
-    return (
-      <div className="CapitalModule">
-        <div className="Measure">
-        <Layout>
-            <Header>
+    // Из массива
+    if (sorters) {
+      gridDataOption.sort[0].fieldName = sorters[0].fieldName;
+      if (sorters[0].sortOrder === "descend") {
+        gridDataOption.sort[0].direction = 1;
+      }
+    }
+    let total = (gridDataOption.pageNumber + 2) * gridDataOption.pageSize;
+    if (total < totalMax) {
+      total = totalMax;
+    } else {
+      setTotalMax(total);
+    }
+    reqwest({
+      url: globalSettings.startURL + "measure/json",
+      contentType: "application/json; charset=utf-8",
+      method: 'post',
+      type: 'json',
+      data:JSON.stringify(gridDataOption)
+    }).then(dataNew => {
+      setLoading(false);
+      setData(dataNew);
+      setPagination({...pagination, total:  total});
+      setSelectedRowKeys([]);
+    });
+    console.log('refreshData - finish');
+  }, [pagination, sorters, totalMax])
+
+  // Эффект - первая выборка
+  React.useEffect(() => {
+    console.log("useEffect - start")
+    if(!data) {
+      setData([]); // важно, иначе начальный refresh выполняется несколько раз
+      refreshData();
+    }
+    console.log("useEffect - finish")
+  }, [data, refreshData]);
+
+  return (
+    <div className="CapitalModule">
+      <div className="Measure">
+      <Layout>
+          <Header>
+          <div>
+              Справочник мер измерения
+          </div>
+          </Header>
             <div>
-                Справочник мер измерения
+              <Menu onClick={handleMenuClick} selectedKeys={[currentMenu]} mode="horizontal">
+                <SubMenu key="Command" title="Команды">
+                    <Menu.Item key="add" icon={<PlusOutlined />}>Добавить</Menu.Item>
+                    <Menu.Item key="edit" icon={<EditOutlined />}>Изменить</Menu.Item>
+                    <Menu.Item key="delete" icon={<CloseOutlined />}>Удалить</Menu.Item>
+                    <Menu.Item key="refresh" icon={<Refresh />}>Обновить</Menu.Item>
+                    <Menu.Item key="print" icon={<PrinterOutlined />}>Печать</Menu.Item>
+                </SubMenu>
+                <SubMenu key="Spravka" title="Справка">
+                  <Menu.Item key="help">Помощь</Menu.Item>
+                    <Menu.Item key="about">
+                      <a href="http://www.gelicon.biz/" target="_blank" rel="noopener noreferrer">
+                        О программе
+                      </a>
+                    </Menu.Item>
+                </SubMenu>
+              </Menu>
             </div>
-            </Header>
+          <Content>
             <div>
-                <Menu onClick={this.handleMenuClick} selectedKeys={[currentMenu]} mode="horizontal">
-                  <SubMenu key="Command" title="Команды">
-                      <Menu.Item key="add" icon={<PlusOutlined />}>Добавить</Menu.Item>
-                      <Menu.Item key="edit" icon={<EditOutlined />}>Изменить</Menu.Item>
-                      <Menu.Item key="delete" icon={<CloseOutlined />}>Удалить</Menu.Item>
-                      <Menu.Item key="refresh" icon={<Refresh />}>Обновить</Menu.Item>
-                      <Menu.Item key="print" icon={<PrinterOutlined />}>Печать</Menu.Item>
-                  </SubMenu>
-                  <SubMenu key="Spravka" title="Справка">
-                    <Menu.Item key="help">Помощь</Menu.Item>
-                      <Menu.Item key="about">
-                        <a href="http://www.gelicon.biz/" target="_blank" rel="noopener noreferrer">
-                          О программе
-                        </a>
-                      </Menu.Item>
-                  </SubMenu>
-                </Menu>
-              </div>
-            <Content>
-              <div>
-                <Table 
-                  rowSelection={rowSelection} 
-                  columns={columns} 
-                  dataSource={data}
-                  pagination={pagination}
-                  loading={loading}
-                  onChange={this.handleTableChange}
-                  rowKey="id"
-                />
-              </div>
-            </Content>
-            <Footer>Низ Капитал</Footer>
-          </Layout>
-        </div>
+              <Table 
+                loading={loading}
+                rowSelection={rowSelection} 
+                columns={columns} 
+                dataSource={data}
+                pagination={pagination}
+                onChange={handleTableChange}
+                rowKey="id"
+              />
+            </div>
+          </Content>
+          <Footer>Низ Капитал</Footer>
+        </Layout>
       </div>
-    );
-  }
+    </div>
+  );
 }
+
+
 export default Measure;
 
 // https://medium.com/@alef.duarte/using-ant-design-form-inside-a-modal-in-react-stateless-functional-component-634f33357c80
