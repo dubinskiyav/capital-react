a95868a363334b6355948fbea8f2e705a7e76e96
dav <dav@gelicon.biz>
2021-02-15 20:47:39 +0500

    unitmeasure

--- src/modules/unitmeasure/Unitmeasure.js
+++ src/modules/unitmeasure/Unitmeasure.js
@@ -2,14 +2,31 @@ import React from 'react';
 import 'antd/dist/antd.css';
 import '../../resources/css/index.css';
 import '../../resources/css/capital.css';
-import { Layout } from 'antd';
+import { Layout, Menu, Modal,Form } from 'antd';
 import { Table } from 'antd';
 import reqwest from 'reqwest';
-import { Space } from 'antd';
+import { PlusOutlined, EditOutlined, CloseOutlined, PrinterOutlined } from '@ant-design/icons';
+import Refresh from '../../icons/Refresh';
+import { notification } from 'antd';
+import { ExclamationCircleOutlined } from '@ant-design/icons';
+import * as globalSettings from "../../lib/const";
+import UnitmeasureForm from "./UnitmeasureForm";
+
+
+// URI для использования формой добавления/изменения
+const URI_ROOT = globalSettings.startURL + "unitmeasure"
+const URI_SELECT = URI_ROOT + "/dto"
+const URI_DEL = URI_ROOT + "/del"
+const URI_ADD = URI_ROOT + "/add"
+const URI_UPD = URI_ROOT + "/upd" // для выборки записи по id
+const URI_POST = URI_ROOT + "/post"
+
 
 const { Header, Content, Footer } = Layout;
 
-// id не надо
+const { SubMenu } = Menu;
+
+// id не надо - он описан в key таблицы
 const columns = [
   {
     title: 'Наименование',
@@ -23,103 +40,339 @@ const columns = [
     sorter: true,
     width: '10%',
   },
+  {
+    title: 'measureunitId',
+    dataIndex: 'measureunitId',
+    sorter: false,
+    width: '10%',
+  },
+  {
+    title: 'Приоритет',
+    dataIndex: 'priority',
+    sorter: true,
+    width: '10%',
+  },
+  {
+    title: 'measureId',
+    dataIndex: 'measureId',
+    sorter: true,
+    width: '10%',
+  },
+  {
+    title: 'Мера измерения',
+    dataIndex: 'measureName',
+    sorter: true,
+    width: '10%',
+  },
 ];
 
-class Unitmeasure extends React.Component {
-  state = {
-    data: [],
-    pagination: {
-      current: 1,
-      pageSize: 10,
-    },
-    selectedRowKeys: [], // Check here to configure the default column
-    loading: false,
-  };
-  // размещаем побочные эффекты
-  componentDidMount() {
-    console.log("componentDidMount - start")
-    const { pagination } = this.state;
-    this.fetch({ pagination });
-    console.log("componentDidMount - finish")
-  }
-  handleTableChange = (pagination, filters, sorter) => {
-    console.log("handleTableChange - start");
-    console.log("sorter.field=" + sorter.field);
-    console.log("sorter.order=" + sorter.order);
-    this.fetch({
-      sortField: sorter.field,
-      sortOrder: sorter.order,
-      pagination,
-      ...filters,
+/**
+ * Компонент для меры измерения
+ * @param {*} props 
+ */
+const Unitmeasure = ()=>{
+  let [currentMenu, setCurrentMenu] = React.useState("null"); // Текущее выбранное меню - непонятно зачем
+  let [selectedRowKeys, setSelectedRowKeys] = React.useState([]); // Отмеченные записи, изначально пустой
+  let [data,setData] = React.useState(null); // Основной массив данных - пустой сначала
+  let [loading,setLoading] = React.useState(false); // Момент загрузки данных для блокировки таблицы для действий
+  let [pagination,setPagination] = React.useState({ // Пагинация таблицы, нумерация с 1
+    current: 1,
+    pageSize: 10,
+    total: null, // общее количество считанных записей
+  });
+  let [sorters, setSorters] = React.useState([{ // Массив сортировки, 
+    fieldName: "id",  // Изначально по id (без сортировки с пагинацией нельзя)
+    sortOrder: "ascend"
+  }]);
+  let [totalMax, setTotalMax] = React.useState(0); // Наибольшее количесвто выбранных записей
+
+  let [formVisible,setFormVisible] = React.useState(false); // Видимость формы ввода
+  let [editorContext] = React.useState({ // Данные для формы добавления-изменения
+    uriForAdd: URI_ADD,
+    uriForUpd: URI_UPD,
+    uriForPost: URI_POST,
+  });
+  const [form] = Form.useForm(); // для эффекта
+
+  /**
+   * Удаление записей
+   */
+  const deleteRows = () => {
+    if (!(selectedRowKeys.length > 0)) {
+      notification.info({
+        message:"Нет записей для удаления",
+        description: "Отметьте одну или несколько записей для удаления"
+      });
+      return;
+    }
+    let ids = selectedRowKeys.join(',');
+    console.log('Deleting records ', ids, ' ...');
+    setLoading(true); // Блокируем отклики таблицы
+    // запрос к REST API на удаление записей
+    // globalSettings.startURL устанавливается в const.js
+    reqwest({
+      url: URI_DEL + '/' + ids,
+      contentType: "application/json; charset=utf-8",
+      method: 'post',
+      type: 'json',
+    }).then((responseJson) => {
+      console.log('responseJson=', responseJson);
+      const { errorCode } = responseJson;
+      if (errorCode) { // Ошибка есть
+        console.log('errorCode=', errorCode);
+        const { errorMessage, fieldErrors } = responseJson;
+        const description = errorMessage + fieldErrors.id;
+        notification.error({
+          message:"Ошибка при удалении записей",
+          description: (description)
+        });
+        } else { // ошибки нет
+          console.log('Удалили ' + ids);
+          refreshData();
+          const description = "Удаление " + selectedRowKeys.length + " записей выполнено успешно";
+          notification.success({
+            message:"Успешно",
+            description: (description)
+          });
+      }
+      setLoading(false);
+      return;
+    }).catch((error) => {
+      setLoading(false);
+      throw (error);
     });
-    console.log("handleTableChange - finish");
+    setLoading(false);
+  }
+
+  /**
+   * Вызов формы добавления-изменения
+   * @param {*} id 
+   */
+  const callForm = (id) => {
+    console.log('calForm - id = ' + id);
+    editorContext.id = id;
+    setFormVisible(true); // Видимость формы
+  }
+
+  /**
+   * Обработчик нажатия меню
+   * @param {*} e 
+   */
+  const handleMenuClick = e => {
+    console.log('click ', e);
+    const { key } = e;
+    currentMenu = key;
+    setCurrentMenu(setCurrentMenu);
+    switch(key) {
+      case 'add':
+        console.log('add');
+        callForm(); // Вызовем форму без установленного параметра id
+        break;
+      case 'delete':
+        if (!(selectedRowKeys.length > 0)) {
+          notification.info({
+            message:"Нет записей для удаления",
+            description: "Отметьте одну или несколько записей для удаления"
+          });
+          return;
+        }
+        const content = "Вы действительно хотите удалить " + selectedRowKeys.length + " записей?";
+        Modal.confirm({
+          title: 'Подтверждение удаления',
+          icon: <ExclamationCircleOutlined />,
+          content: (content),
+          okText: 'Да, конечно!',
+          cancelText: 'Нет, я ошибся',
+          onOk:()=>{
+            deleteRows();
+          }
+        });
+        break;
+      case 'refresh':
+        refreshData();
+        break;
+      case 'print':
+          notification.success({
+            message:"Успешно",
+            description:"Удаление записей выполнено успешно"
+          });
+            break;
+      default:
+        console.log('Не реализовано ');
+    }
   };
 
-  fetch = (params = {}) => {
-    console.log("fetch - start");
-    this.setState({ loading: true });
+  /**
+   * Сохранение отметки записей
+   * @param {*} selectedRowKeysNew 
+   */
+  const onSelectChange = selectedRowKeysNew => {
+    setSelectedRowKeys( selectedRowKeysNew );
+  };
+
+  /**
+   * Отмеченные записи 
+   */
+  const rowSelection = {
+    selectedRowKeys,
+    onChange: onSelectChange,
+  };
+
+  /**
+   * Обработчик смены параметров запроса из таблицы
+   * @param {*} paginationNew // При смене пагинации
+   * @param {*} filters // фильтра
+   * @param {*} sorter  // сортировки
+   */
+  const handleTableChange = (paginationNew, filters, sorter) => {
+    console.log('handleTableChange - start');
+    if (sorter.field) {
+      sorters = [{
+        fieldName: sorter.field, 
+        sortOrder: sorter.order
+      }];
+      setSorters(sorters);
+    }  
+    pagination = paginationNew;
+    setPagination(pagination);
+    refreshData();
+    console.log('handleTableChange - finish');
+  };
+
+  /**
+   * Основная перевыборка данных
+   */
+  const refreshData = () => {
+    console.log('refreshData - start');
+    setLoading(true);
     const gridDataOption = {
-      pageNumber: params.pagination.current - 1,
-      pageSize: params.pagination.pageSize,
-      sort: [{fieldName: "id"}]
+      pageNumber: pagination.current - 1,
+      pageSize: pagination.pageSize,
+      sort: [{fieldName: "id"}] // Сортировка по умолчанию
     };
-    if (params.sortField) {
-      console.log("params.sortField=" + params.sortField);
-      gridDataOption.sort[0].fieldName = params.sortField;
-      if (params.sortOrder === "descend") {
+    if (sorters) { // Сортировка установлена - переустановим
+      gridDataOption.sort[0].fieldName = sorters[0].fieldName;
+      if (sorters[0].sortOrder === "descend") {
         gridDataOption.sort[0].direction = 1;
-        console.log("params.sortOrder=descend");
       }
     }
-    // ajax request after empty completing
+    console.log('gridDataOption=' + JSON.stringify(gridDataOption));
+    // Вычисляем total для таблицы
+    let total = (gridDataOption.pageNumber + 2) * gridDataOption.pageSize;
+    if (total < totalMax) {
+      total = totalMax;
+    } else {
+      setTotalMax(total);
+    }
+    // запрос к REST API на выборку
     reqwest({
-      url: 'http://localhost:8080/unitmeasure/json',
+      url: URI_SELECT,
       contentType: "application/json; charset=utf-8",
       method: 'post',
       type: 'json',
       data:JSON.stringify(gridDataOption)
-    }).then(data => {
-      //console.log(data);
-      this.setState({
-        loading: false,
-        data: data,
-        pagination: {
-          ...params.pagination,
-          total: 200,
-          // 200 is mock data, you should read it from server
-          // total: data.totalCount,
-        },
-      });
-      console.log("fetch - finish");
-    });
-  };
-  
-
-  render() {
-    console.log("render - start");
-    const { data, pagination, loading } = this.state;
-    return (
-      <div className="CapitalModule">
-        <div className="Unitmeasure">
-        <Layout>
-            <Header>Справочник единиц измерения</Header>
-            <Content>
-              <div>
-                <Table 
-                  columns={columns} 
-                  dataSource={data}
-                  pagination={pagination}
-                  loading={loading}
-                  onChange={this.handleTableChange}
-                  rowKey="id"
-                />
-              </div>
-            </Content>
-            <Footer>Низ Капитал</Footer>
-          </Layout>
-        </div>
-      </div>
+    })
+      .then(dataNew => {
+        setData(dataNew); // данные новые
+        setPagination({...pagination, total:  total}); // переустановим total у таблицы
+        setSelectedRowKeys([]); // обнулим отмеченные
+      },
+      // todo Сделать обработку ошибок
+      (error) => {
+        notification.error({
+          message:"Ошибка при выборке за пределами программы",
+          description: "error"
+        });
+        console.log('refreshData - error=' + error);
+      }
     );
+    setLoading(false);
+    console.log('refreshData - finish');
   }
+
+  /**
+   * Используем побочный эффект для первоначальной выборки 
+   */
+  React.useEffect(() => {
+    if(!data) {      // Делаем только если данных нет, иначе начальный refresh выполняется бесконечно
+      console.log("useEffect - initial refresh")
+      setData([]); // Непонятно зачем
+      refreshData();
+      console.log("useEffect - initial refreshed")
+    }
+  }); 
+
+  /**
+   * Возвращаем React компонент
+   */
+  return (
+    <div className="CapitalModule">
+      <div className="Measure">
+      <Layout>
+          <Header>
+            Список мер измерения
+          </Header>
+            <div>
+              <Menu onClick={handleMenuClick} selectedKeys={[currentMenu]} mode="horizontal">
+                <SubMenu key="Command" title="Команды">
+                    <Menu.Item key="add" icon={<PlusOutlined />}>Добавить</Menu.Item>
+                    <Menu.Item key="edit" icon={<EditOutlined />}>Изменить</Menu.Item>
+                    <Menu.Item key="delete" icon={<CloseOutlined />}>Удалить</Menu.Item>
+                    <Menu.Item key="refresh" icon={<Refresh />}>Обновить</Menu.Item>
+                    <Menu.Item key="print" icon={<PrinterOutlined />}>Печать</Menu.Item>
+                </SubMenu>
+                <SubMenu key="Spravka" title="Справка">
+                  <Menu.Item key="help">Помощь</Menu.Item>
+                    <Menu.Item key="about">
+                      <a href="http://www.gelicon.biz/" target="_blank" rel="noopener noreferrer">
+                        О программе
+                      </a>
+                    </Menu.Item>
+                </SubMenu>
+              </Menu>
+            </div>
+          <Content>
+            <div>
+              <Table 
+                className="mod-main-table" 
+                rowClassName="table-editable-row"  
+                bordered 
+                size={"middle"}
+                loading={loading}
+                rowSelection={rowSelection} 
+                columns={columns} 
+                dataSource={data}
+                pagination={pagination}
+                onChange={handleTableChange}
+                rowKey="id"
+                onRow={(record, rowIndex) => {
+                  return {
+                    onClick: event => callForm(record.id)
+                  };
+                }}
+              />
+            </div>
+            <MeasureForm 
+              form={form}
+              visible={formVisible}
+              editorContext={editorContext}
+              afterCancel = {() => {
+                setFormVisible(false);
+              }}
+              afterSave={()=>{
+                setFormVisible(false);
+                refreshData();
+              }}
+            >
+            </MeasureForm>
+          </Content>
+          <Footer>Низ Капитал</Footer>
+        </Layout>
+      </div>
+    </div>
+  );
 }
-export default Unitmeasure;
+
+export default Measure;
+
+// https://medium.com/@alef.duarte/using-ant-design-form-inside-a-modal-in-react-stateless-functional-component-634f33357c80
---
+++ src/modules/unitmeasure/UnitmeasureForm.js
@@ -0,0 +1,183 @@
+import React from 'react';
+import {Modal} from 'antd';
+import { Form, Input } from 'antd';
+import reqwest from 'reqwest';
+import { notification } from 'antd';
+
+const titleAdd = "Добавление меры измерения";
+const titleUpd = "Изменение меры измерения";
+
+/**
+ * Добавление-изменение меры измерения
+ * @param {*} props 
+ */
+const UnitmeasureForm = (props)=>{
+    const firstInputRef = React.useRef(null); // Создаем реф для первоначального фокуса
+    let [data,setData] = React.useState(null); // Запись для редактирования
+    let form = props.form; // Форма из параметра для эффекта
+
+    /**
+     * Загрузка данных из id
+     */
+    const load = ()=>{
+        console.log("Load with props.editorContext.id=" + props.editorContext.id);
+        if(props.editorContext.id && props.editorContext.id !== 0) {
+            console.log("Load for edit id = ",props.editorContext.id);
+            console.log("uriForUpd = ",props.editorContext.uriForUpd);
+            // запрос к REST API на выборку по id
+            reqwest({
+                url: props.editorContext.uriForUpd + '/' + props.editorContext.id,
+                contentType: "application/json; charset=utf-8",
+                method: 'get',
+                type: 'json',
+            }).then(record => {
+                    console.log("record = " + JSON.stringify(record));
+                    setData(record); // данные новые
+                    //form.resetFields(); // Перенесено в useEffect
+                },
+                // todo Сделать обработку ошибок
+                (error) => {
+                notification.error({
+                    message:"Ошибка при выборке за пределами программы",
+                    description: "error"
+                });
+                console.log('refreshData - error=' + error);
+                }
+            );
+        } else {
+            console.log("Load for add");
+            // запрос к REST API на выборку для начального значения
+            reqwest({
+                url: props.editorContext.uriForAdd,
+                contentType: "application/json; charset=utf-8",
+                method: 'get',
+                type: 'json',
+            }).then(record => {
+                    console.log("record = " + JSON.stringify(record));
+                    setData(record); // данные новые
+                },
+                (error) => {
+                notification.error({
+                    message:"Ошибка при выборке за пределами программы",
+                    description: "error"
+                });
+                console.log('refreshData - error=' + error);
+                }
+            );
+        }
+    }
+   
+    if(!data && props.visible) { // Если нет данных про запись и видима - грузим
+        load();
+    }
+
+    // Побочный эффект для первоначального фокуса
+    React.useEffect(()=>{
+        if (props.visible) { // Обязательно, если видимость, иначе свалится
+            form.resetFields(); // Для того, чтобы поля перерисовались со значениями
+            // form = props.form;
+            setTimeout(() => { // На всякий случай таймаут
+                firstInputRef.current.focus({cursor: 'end',});
+            }, 100);
+        }
+    })
+
+    function handleClick() {
+        console.log("handleClick - form.resetFields();");
+        form.resetFields();
+    }
+
+    //Сохранение
+    const save=(values,after)=>{
+        const record = { // сцепим id и vslues в один объект
+            "id": props.editorContext.id,
+            ...values
+        }
+        console.log("Saving record = ",JSON.stringify(record));
+        // запрос к REST API на выборку
+        reqwest({
+            url: props.editorContext.uriForPost,
+            contentType: "application/json; charset=utf-8",
+            method: 'post',
+            type: 'json',
+            data:JSON.stringify(record)
+        }).then((responseJson) => {
+            console.log('responseJson=', responseJson);
+            const { errorCode } = responseJson;
+            if (errorCode) { // Ошибка есть
+              console.log('errorCode=', errorCode);
+              const { errorMessage, fieldErrors } = responseJson;
+              const description = errorMessage + fieldErrors.id;
+              notification.error({
+                message:"Ошибка при сохранении записей",
+                description: (description)
+              });
+              } else { // ошибки нет
+                after(); // Сюда передали refreshData
+                let description = "Запись добавлена";
+                if (props.editorContext.id) {
+                    description = "Запись изменена";
+                }
+                notification.success({
+                  message:"Успешно",
+                  description: (description)
+                });
+            }
+        })
+    }
+    
+
+    return <Modal
+        visible={props.visible}
+        title="Новая запись"
+        title={ props.editorContext.id ? titleUpd : titleAdd }
+        okText="Согласен"
+        cancelText="Отмена"
+        onCancel={()=>{
+            props.afterCancel();
+            setData(null);
+        }}
+        onOk={() => {
+            props.form.validateFields().then((values) => {
+                    save(values,()=>{
+                        props.afterSave(values);
+                        setData(null);
+                    });
+                })
+                .catch((info) => {
+                    console.log('Validate Failed:', info);
+                    notification.error({
+                        message: 'Ошибка ввода',
+                        description: 'Проверьте правильность ввода всех полей'
+                    });
+                });
+            }
+        }
+        >
+        <Form
+            form={form}
+            layout="horizontal"
+            name="measureForm"
+            initialValues={data}
+        >
+            <Form.Item
+                name="name"
+                label="Наименование"
+                rules={[
+                    {
+                        required: true,
+                        message: 'Необходимо ввести наименование',
+                    },
+                ]}>
+                <Input ref={firstInputRef} style={{width1:300}}/>
+            </Form.Item>
+            <input
+                type="button"
+                value="Не нажимай!"
+                onClick={handleClick}
+            />
+        </Form>
+  </Modal>
+}    
+
+export default UnitmeasureForm;
